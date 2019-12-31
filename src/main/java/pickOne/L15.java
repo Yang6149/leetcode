8ae23531791e6abd38d1ab7a321c85c6fa85d5f0
Yang shen <614989768@qq.com>
2019-12-31 20:59:35 +0800

    Remove Nth Node From End of List

--- src/main/java/pickOne/L15.java
+++ src/main/java/pickOne/L15.java
@@ -11,7 +11,38 @@ public class L15 {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> lists=new ArrayList<>();
         Arrays.sort(nums);
-        return null;
+        for(int i=0;i<nums.length-2;i++){
+            if(i!=0){
+                if(nums[i]==nums[i-1]){
+                    continue;
+                }
+            }
+            int begin=i+1;
+            int end=nums.length-1;
+            while(begin<end){
+                if(begin!=i+1&&nums[begin]==nums[begin-1]){
+                    begin++;
+                    continue;
+                }
+                int sum = nums[begin]+nums [end];
+                if (sum + nums[i]==0){
+                    List<Integer>numsList =new ArrayList<Integer>();
+                    numsList.add(nums[begin]);
+                    numsList.add(nums[end]);
+                    numsList.add(nums[i]);
+                    lists.add(numsList);
+                    begin ++;
+                    end --;
+                    continue;
+                }else if(sum+nums[i]<0){
+                    begin ++;
+                }
+                else {
+                    end --;
+                }
+            }
+        }
+        return lists;
 
     }
 
