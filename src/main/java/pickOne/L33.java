39086b83fd58fb8052e37bc95e2eb522ad2ebe87
Yang shen <614989768@qq.com>
2020-01-02 22:35:56 +0800

    改进

--- src/main/java/pickOne/L33.java
+++ src/main/java/pickOne/L33.java
@@ -3,38 +3,32 @@ package pickOne;
 import org.junit.Test;
 
 public class L33 {
-    public int[] searchRange(int[] nums, int target) {
+    public int[] searchRange(int[] A, int target) {
+        int start = findFirst(A, target);
+        if (start == A.length || A[start] != target) {
+            return new int[]{-1, -1};
+        }
+        return new int[]{start, findFirst(A, target + 1) - 1};
+
+    }
+    public int findFirst(int[] nums, int target){
         int tail=nums.length-1;
         int head=0;
-        while(head<tail){
-            int mid=(head+tail)/2;
-            if(nums[mid]==target){
-                tail=mid;
-                head=mid;
-            }
-            if(nums[mid]>target){
+        int index=-1;
+        while(tail>=head){
+            int mid=(tail+head)/2;
+            if(nums[mid]>=target){
                 tail=mid-1;
-            }
-            if(nums[mid]<target){
+            }else{
                 head=mid+1;
             }
-        }
-        if(nums.length<1||nums[head]!=target)return new int[]{-1,-1};
-        //head=tail= index of target
-        while(nums[head]==target){
-            head--;
-            if(head<0)break;
-        }
-        while(nums[tail]==target){
-            tail++;
-            if(tail==nums.length)break;
-        }
-        return new int[]{head+1,tail-1};
-    }
 
+        }
+        return head;
+    }
     @Test
     public void test(){
-        for(int i:searchRange(new int[]{5,7,7,10},8)){
+        for(int i:searchRange(new int[]{5,7,7,10},7)){
             System.out.println(i);
         }
     }
