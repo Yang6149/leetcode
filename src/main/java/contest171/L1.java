68b11379fff03e6a0de2e70201da99fdfd6b979c
Yang shen <614989768@qq.com>
2020-01-17 00:54:35 +0800

    96

---
+++ src/main/java/contest171/L1.java
@@ -0,0 +1,29 @@
+package contest171;
+
+import org.junit.Test;
+
+public class L1 {
+    public int[] getNoZeroIntegers(int n) {
+        for(int i=1;i<n/2+1;i++){
+            int res=n-i;
+            if(iszero(res)&&iszero(i))return new int[]{i,res};
+        }
+        return new int[2];
+    }
+    public boolean iszero(int n){
+        while(n>0){
+            if(n%10==0){
+                return false;
+            } else{
+                n/=10;
+            }
+        }
+        return true;
+    }
+    @Test
+    public void test(){
+        for (int noZeroInteger : getNoZeroIntegers(2)) {
+            System.out.println(noZeroInteger);
+        }
+    }
+}
---
+++ src/main/java/contest171/L3.java
@@ -0,0 +1,19 @@
+package contest171;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.*;
+
+/**
+ *  i will go back
+ */
+public class L3 {
+    public int makeConnected(int n, int[][] connections) {
+        return 0;
+    }
+    @Test
+    public void test(){
+
+    }
+}
