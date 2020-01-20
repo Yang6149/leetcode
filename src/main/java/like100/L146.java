b105eb6505b84f8e905fdca7ed2a318a07a129f5
Yang shen <614989768@qq.com>
2020-01-20 22:38:59 +0800

    L146 修改

--- src/main/java/like100/L146.java
+++ src/main/java/like100/L146.java
@@ -1,10 +1,14 @@
 package like100;
 
+import org.junit.Test;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class L146 {
+    HashMap<Integer,Nooo> map=new HashMap<>();
     int capacity;
     int size=0;
     Nooo head;
@@ -18,48 +22,81 @@ public class L146 {
         int value;
         Nooo next;
         Nooo pre;
+
+        @Override
+        public String toString() {
+            return "Nooo{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
     }
     LinkedList<Integer> linkedList;
     public L146(int capacity) {
         this.capacity=capacity;
+        head=new Nooo(0,0);
+        teal=new Nooo(0,0);
+        head.next=teal;
+        teal.pre=head;
     }
 
-    public int get(int key) {
-        Nooo index=head;
-        while (index!=null){
-            if(index.key==key){
-                if(index!=head){
-                    index.pre.next=index.next;
-                    index.next.pre=index.pre;
-                    index.next=head;
-                    head.pre=index;
-                    head=index;
-                }
-                return index.value;
+    public void remove(int key){
+        Nooo cur=map.remove(key);
 
-            }
-            index=index.next;
-        }
-        return -1;
+        cur.next.pre=cur.pre;
+        cur.pre.next=cur.next;
+
+        size--;
+    }
+    public int get(int key) {
+        Nooo res=map.get(key);
+        if(res==null)return -1;
+        remove(key);
+        put(key,res.value);
+        return res.value;
     }
 
     public void put(int key, int value) {
-        if(size==0){
-            head= new Nooo(key,value);
-            teal=head;
-            head.next=head;
-            head.pre=head;
+        if(get(key)!=-1){
+            remove(key);
         }
         if(size==capacity){
-            teal=teal.pre;
-            teal.next=null;
-        }
-        if(size<capacity){
-            size++;
+            remove(teal.pre.key);
         }
         Nooo cur=new Nooo(key,value);
-        head.pre=cur;
-        cur.next=head;
-        head=cur;
+
+        cur.next=head.next;
+        cur.next.pre=cur;
+        head.next=cur;
+        cur.pre=head;
+
+        map.put(key,cur);
+        size++;
+    }
+
+    @Test
+    public void test(){
+        L146 l=new L146(2);
+        l.put(1,1);
+        l.put(2,2);
+        l.get(1);
+        l.put(3,3);
+        System.out.println(l.get(2));
+    }
+
+    public static void main(String[] args) {
+        L146 cache=new L146(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        cache.get(1);       // returns 1
+        cache.put(3, 3);    // evicts key 2
+        cache.get(2);       // returns -1 (not found)
+        cache.put(4, 4);    // evicts key 1
+        cache.get(1);       // returns -1 (not found)
+        cache.get(3);       // returns 3
+        cache.get(4);
+        System.out.println(cache.get(4));
+        System.out.println();
     }
 }
