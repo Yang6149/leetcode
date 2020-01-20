f19aa6d112ba16d38d0400b0437c399edf89fb42
Yang shen <614989768@qq.com>
2020-01-21 02:22:32 +0800

    mergeSort 节省空间

--- src/main/java/sort/mergeSort.java
+++ src/main/java/sort/mergeSort.java
@@ -5,23 +5,23 @@ import org.junit.Test;
 import java.util.Random;
 
 public class mergeSort {
-    public void sort(int [] nums,int begin,int mid,int end){
+    public void sort(int [] nums,int begin,int mid,int end,int [] copy){
         if(begin==end)return;
-        sort(nums,begin,(begin+mid)/2,mid);
-        sort(nums,mid+1,(mid+end+1)/2,end);
+        sort(nums,begin,(begin+mid)/2,mid,copy);
+        sort(nums,mid+1,(mid+end+1)/2,end,copy);
         //System.out.println("begin:"+begin+"  mid:"+mid+"  end:"+end);
         int index1=begin;
         int index2=mid+1;
-        int [] copy=new int[end-begin+1];
         int index=0;
-        while (index<copy.length){
+        while (index<end-begin+1){
             if(index2==end+1||(index1<mid+1&&nums[index1]<=nums[index2])){
                 copy[index++]=nums[index1++];
             }else{
                 copy[index++]=nums[index2++];
             }
         }
-        for(int i=0;i<copy.length;i++){
+        int f=end-begin+1;
+        for(int i=0;i<f;i++){
             nums[begin++]=copy[i];
         }
     }
@@ -31,9 +31,9 @@ public class mergeSort {
         int [] num=new int[100];
         Random r=new Random();
         for(int i=0;i<num.length;i++){
-            num[i]= r.nextInt(100);
+            num[i]= r.nextInt(10000);
         }
-        sort(num,0,(num.length-1)/2,num.length-1);
+        sort(num,0,(num.length-1)/2,num.length-1,new int[num.length]);
         for (int i : num) {
             System.out.print(i+"\t");
         }
