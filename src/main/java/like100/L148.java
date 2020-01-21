43102c591329f22ba3ec1cf054fd919b6e3632c8
Yang shen <614989768@qq.com>
2020-01-21 16:24:13 +0800

    sort linkedlist

--- src/main/java/like100/L148.java
+++ src/main/java/like100/L148.java
@@ -1,19 +1,69 @@
 package like100;
 
+import org.junit.Test;
+
 public class L148 {
     public ListNode sortList(ListNode head) {
-//        ListNode f=new ListNode(0);
-//        f.next=head;
-//        while(head.next.next!=null)head=head.next;
-//        Qsort(f,head);
-        return null;
+        if (head.next==null){
+            return head;
+        }
+        //1 分
+        ListNode pre=head,slow=head,fast=head;//1-2-3
+        while (fast!=null&&fast.next!=null){
+            pre=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        pre.next=null;
+        ListNode left=head;
+        //2
+        left = sortList(left);
+        ListNode right=sortList(slow);
+        //合
+        return merge(left,right);
     }
 
-    public void MergeSort(ListNode head,ListNode teal,int gap,int n){
-        //分开
-        //排序
-        //结合
+    public ListNode merge(ListNode left,ListNode right){
+        ListNode index1=left;
+        ListNode index2=right;
+        ListNode res;
+        if(index2==null||(index1!=null&&index1.val<=index2.val)){
+            res=index1;
+            index1=index1.next;
+        }else{
+            res=index2;
+            index2=index2.next;
+        }
+        ListNode resss=res;
+        while (index1!=null||index2!=null){
+            if(index2==null||(index1!=null&&index1.val<=index2.val)){
+                res.next=index1;
+                index1=index1.next;
+                res=res.next;
+            }else{
+                res.next=index2;
+                index2=index2.next;
+                res=res.next;
+            }
+        }
+        return resss;
+    }
 
 
+
+    @Test
+    public void test(){
+        ListNode a = new ListNode(4);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(1);
+        ListNode d = new ListNode(6);
+        ListNode e = new ListNode(3);
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        System.out.println(a);
+
+        System.out.println(sortList(a));
     }
 }
