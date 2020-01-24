a1dee7fb0f73e07d46c6b7ab82c35d3c952a80be
Yang shen <614989768@qq.com>
2020-01-24 23:45:52 +0800

    寻找 Perfect Squares  target包含最少数量的平方

--- src/main/java/like100/L279.java
+++ src/main/java/like100/L279.java
@@ -3,15 +3,31 @@ package like100;
 import org.junit.Test;
 
 public class L279 {
+    static int [] num=new int[1];
     public int numSquares(int n) {
-        int res=0;
+        int begin=num.length;
+        if(n>=num.length){
+            int [] newNums=new int[n+1];
+            for (int i=0;i<num.length;i++){
+                newNums[i]=num[i];
+            }
+            num=newNums;
+        }
 
-        return res;
+        for(int i=begin;i<=n;i++){
+            int min=Integer.MAX_VALUE;
+            for(int j=1;j*j<=i;j++){
+                min=Math.min(num[i-j*j]+1,min);
+            }
+            num[i]=min;
+        }
+        return num[n];
     }
 
     @Test
     public void test(){
-        System.out.println(numSquares(12));
+        System.out.println(numSquares(4));
+        System.out.println(numSquares(5));
     }
 
 }
