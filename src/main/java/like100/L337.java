1d09f2b61d83b71bdbfa5cb2b3683054a57b439c
Yang shen <614989768@qq.com>
2020-01-27 22:11:26 +0800

    robber III

--- src/main/java/like100/L337.java
+++ src/main/java/like100/L337.java
@@ -8,11 +8,24 @@ import java.util.Deque;
 
 public class L337 {
     public int rob(TreeNode root) {
-
-        return 0;
+        if(root==null)return 0;
+        int LL=0,LR=0,RL=0,RR=0,L=0,R=0;
+        if (root.left!=null){
+            L=rob(root.left);
+            LL=root.left.left!=null?root.left.left.val:0;
+            LR=root.left.right!=null?root.left.right.val:0;
+        }
+        if (root.right!=null){
+            R=rob(root.right);
+            RL=root.right.left!=null?root.right.left.val:0;
+            RR=root.right.right!=null?root.right.right.val:0;
+        }
+        root.val=Math.max(L+R,LL+LR+RL+RR+root.val);
+        return root.val;
     }
     @Test
     public void test(){
-
+        TreeNode a=TreeNode.stringToTreeNode("[1,2,3,null,5,8,7,6,1,null,6,9,8,4,2]");
+        System.out.println(rob(a));
     }
 }
