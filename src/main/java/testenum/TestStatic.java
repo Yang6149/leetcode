d27fbc518ab85eb527571199756e9b820d234583
Yang shen <614989768@qq.com>
2020-02-04 18:18:32 +0800

    不能通过实例类访问静态成员

--- src/main/java/testenum/TestStatic.java
+++ src/main/java/testenum/TestStatic.java
@@ -4,10 +4,14 @@ import org.junit.Test;
 
 public class TestStatic {
     public static int a=2;
+    public static void sot(){
+        System.out.println(a);
+    }
     @Test
     public void test(){
         System.out.println(TestStatic.a);
-//        TestStatic b = new TestStatic();
+        TestStatic b = new TestStatic();
+//        b.sot();
 //        System.out.println(b.a);
     }
 }
