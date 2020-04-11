8aef6884bb54bcbbecb5b3627ee71dcd57004429
Yang shen <614989768@qq.com>
2020-04-11 13:29:28 +0800

    每日

--- src/main/java/everydayLeet/L121.java
+++ src/main/java/everydayLeet/L121.java
@@ -1,4 +1,14 @@
 package everydayLeet;
 
 public class L121 {
+    public int maxProfit(int[] prices) {
+        if(prices.length<1)return 0;
+        int min = prices[0];
+        int res = 0;
+        for(int i = 0;i<prices.length;i++){
+            min = Math.min(prices[i],min);
+            res = Math.max(res,prices[i]-min);
+        }
+        return res;
+    }
 }
